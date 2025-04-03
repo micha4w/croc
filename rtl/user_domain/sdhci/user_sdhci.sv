194b8d126cb27c98237c716f98888d8e6fae50b6
micha4w <micha4w@gmail.com>
2025-04-04 00:02:06 +0200

    feat: add basic sram shift register

--- rtl/user_domain/sdhci/user_sdhci.sv
+++ rtl/user_domain/sdhci/user_sdhci.sv
@@ -3,79 +3,78 @@
 // SPDX-License-Identifier: SHL-0.51
 
 `include "common_cells/registers.svh"
+`include "register_interface/typedef.svh"
 
 module user_sdhci #(
-  parameter obi_pkg::obi_cfg_t           ObiCfg      = obi_pkg::ObiDefaultConfig,
-  parameter type                         obi_req_t   = logic,
-  parameter type                         obi_rsp_t   = logic
+  parameter obi_pkg::obi_cfg_t ObiCfg      = obi_pkg::ObiDefaultConfig,
+  parameter type               obi_req_t   = logic,
+  parameter type               obi_rsp_t   = logic
 ) (
   input  logic clk_i,
   input  logic rst_ni,
 
   input  obi_req_t obi_req_i,
-  output obi_rsp_t obi_rsp_o
+  output obi_rsp_t obi_rsp_o, 
+
+  output logic interrupt_o
 );
+  `REG_BUS_TYPEDEF_ALL(
+    reg,
+    logic [ObiCfg.AddrWidth-1:0],
+    logic [ObiCfg.DataWidth-1:0],
+    logic [ObiCfg.AddrWidth/8-1:0]
+  )
+  reg_req_t reg_req;
+  reg_rsp_t reg_rsp;
 
-`include "register_interface/typedef.svh"
-`include "register_interface/assign.svh"
+  obi_to_reg #(
+    .DATA_WIDTH (ObiCfg.DataWidth),
+    .ID_WIDTH   (ObiCfg.IdWidth),
 
-// Simple translation from OBI to RegBus, only works because we know that sdhci_reg_top always gives an answer in the next clock cycle
-// TODO: replace with an implementation in regtool
-  localparam int unsigned AW = sdhci_reg_pkg::BlockAw;
-  localparam int unsigned DW = 32;
-  localparam int unsigned STRB_WIDTH = DW/8;
+    .obi_req_t (obi_req_t),
+    .obi_rsp_t (obi_rsp_t),
+    .reg_req_t (reg_req_t),
+    .reg_rsp_t (reg_rsp_t)
+  ) i_obi_to_reg (
+    .clk_i,
+    .rst_ni,
 
-  typedef logic [AW-1:0] addr_t;
-  typedef logic [DW-1:0] data_t;
-  typedef logic [STRB_WIDTH-1:0] strb_t;
-  `REG_BUS_TYPEDEF_ALL(reg_bus, addr_t, data_t, strb_t)
-
-  reg_bus_req_t s_reg_req;
-  reg_bus_rsp_t s_reg_rsp;
-
-  assign s_reg_req.addr  = obi_req_i.a.addr;
-  assign s_reg_req.write = obi_req_i.a.we;
-  assign s_reg_req.wdata = obi_req_i.a.wdata;
-  assign s_reg_req.wstrb = obi_req_i.a.be;
-  assign s_reg_req.valid = obi_req_i.req;
-
-  logic [ObiCfg.IdWidth : 0] id_q, id_d;
-  `FF(id_q, id_d, '0, clk_i, rst_ni);
-  assign id_d            = obi_req_i.a.aid;
-  assign obi_rsp_o.r.rid = id_q;
-
-  logic valid_q, valid_d;
-  `FF(valid_q, valid_d, '0, clk_i, rst_ni);
-  assign valid_d          = obi_req_i.req;
-  assign obi_rsp_o.rvalid = valid_q;
-
-  assign obi_rsp_o.gnt          = '1;
-  assign obi_rsp_o.r.r_optional = '0;
-
-  logic [ObiCfg.DataWidth-1:0] rdata_q, rdata_d;
-  `FF(rdata_q, rdata_d, '0, clk_i, rst_ni);
-  assign rdata_d           = s_reg_rsp.rdata;
-  assign obi_rsp_o.r.rdata = rdata_q;
-
-  logic error_q, error_d;
-  `FF(error_q, error_d, '0, clk_i, rst_ni);
-  assign error_d         = s_reg_rsp.error;
-  assign obi_rsp_o.r.err = error_q;
+    .obi_req_i (obi_req_i),
+    .obi_rsp_o (obi_rsp_o),
+    .reg_req_o (reg_req),
+    .reg_rsp_i (reg_rsp)
+  );
 
 
 // Actuall SDHCI logic
   sdhci_reg_pkg::sdhci_reg2hw_t reg2hw;
   sdhci_reg_pkg::sdhci_hw2reg_t hw2reg;
+  
+  sram_shift_reg i_shift (
+    .clk_i,
+    .rst_ni,
 
+    .req_i (reg2hw.buffer_data_port.re | reg2hw.buffer_data_port.qe),
+    .we_i  (reg2hw.buffer_data_port.qe),
+
+    .read_data_o  (hw2reg.buffer_data_port.d),
+    .write_data_i (reg2hw.buffer_data_port.q),
+    
+    .full_o  (),
+    .empty_o ()
+  );
+
+  
   sdhci_reg_top #(
-    .reg_req_t(reg_bus_req_t),
-    .reg_rsp_t(reg_bus_rsp_t),
-    .AW(AW)
+    .AW        (ObiCfg.AddrWidth),
+
+    .reg_req_t (reg_req_t),
+    .reg_rsp_t (reg_rsp_t)
   ) i_regs (
     .clk_i,
     .rst_ni,
-    .reg_req_i(s_reg_req),
-    .reg_rsp_o(s_reg_rsp),
+    .reg_req_i (reg_req),
+    .reg_rsp_o (reg_rsp),
     .reg2hw,
     .hw2reg,
     .devmode_i ('b1)
