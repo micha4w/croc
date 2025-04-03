c484d5f59cb314e88c7b466bbc6da21e0fcb7914
Anton Buchner <abuchner@student.ethz.ch>
2025-04-04 00:09:15 +0200

    removed verilog obj_dir

--- verilator/obj_dir/Vtb_croc_soc___024root__DepSet_h0ddea910__0__Slow.d
+++
@@ -1,3 +0,0 @@
-Vtb_croc_soc___024root__DepSet_h0ddea910__0__Slow.o: \
- Vtb_croc_soc___024root__DepSet_h0ddea910__0__Slow.cpp \
- Vtb_croc_soc__pch.h Vtb_croc_soc__Syms.h Vtb_croc_soc___024root.h
--- verilator/obj_dir/Vtb_croc_soc___024root__DepSet_ha6051a75__0__Slow.d
+++
@@ -1,3 +0,0 @@
-Vtb_croc_soc___024root__DepSet_ha6051a75__0__Slow.o: \
- Vtb_croc_soc___024root__DepSet_ha6051a75__0__Slow.cpp \
- Vtb_croc_soc__pch.h Vtb_croc_soc___024root.h
