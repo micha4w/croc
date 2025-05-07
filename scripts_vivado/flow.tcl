a1a0d320bb1bc04870c90308655286da3556d211
Anton Buchner <abuchner@student.ethz.ch>
2025-05-07 17:53:32 +0200

    added scripts, modified makefile

---
+++ scripts_vivado/flow.tcl
@@ -0,0 +1,26 @@
+# Define common variables
+source ../scripts/common.tcl
+
+# Create project
+source ${scripts_dir}/init.tcl
+
+# Add and compile clock wizard IP
+source ${scripts_dir}/clkwiz.tcl
+
+# Elaborate
+synth_design -top ${top} -part ${device} -lint
+synth_design -rtl -rtl_skip_mlo -name rtl_1
+
+# Synthesize
+# set synth_strategy Flow_RuntimeOptimized
+launch_runs synth_1 -jobs ${numjobs}
+wait_on_run synth_1
+
+# Implement
+# set impl_strategy Flow_RuntimeOptimized
+launch_runs impl_1 -jobs ${numjobs}
+wait_on_run impl_1
+
+# Generate bitstream
+launch_runs impl_1 -to_step write_bitstream -jobs ${numjobs}
+wait_on_run impl_1
