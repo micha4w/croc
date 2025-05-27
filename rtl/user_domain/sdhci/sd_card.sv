4eec24cabd621ddd291c96b0ad534318763e759f
micha4w <micha4w@gmail.com>
2025-05-28 00:11:00 +0200

    model: add hold time

--- rtl/user_domain/sdhci/sd_card.sv
+++ rtl/user_domain/sdhci/sd_card.sv
@@ -10,12 +10,20 @@ module sd_card(
     output logic [3:0] dat_o
 );
   tri1 cmd;
-  assign cmd = cmd_en_i ? cmd_i : 'z;
-  assign cmd_o = cmd;
-  
+  logic delayed_cmd_en;
+  logic delayed_cmd;
+  assign #(3ns) delayed_cmd_en = cmd_en_i;
+  assign #(3ns) delayed_cmd    = cmd_i;
+  assign cmd = delayed_cmd_en ? delayed_cmd : 'z;
+  assign #(3ns) cmd_o = cmd;
+
   tri1 [3:0] dat;
-  assign dat = dat_en_i ? dat_i : 'z;
-  assign dat_o = dat;
+  logic delayed_dat_en;
+  logic [3:0] delayed_dat;
+  assign #(3ns) delayed_dat_en = dat_en_i;
+  assign #(3ns) delayed_dat    = dat_i;
+  assign dat = delayed_dat_en ? delayed_dat : 'z;
+  assign #(3ns) dat_o = dat;
 
   sdModel i_model (
     .sdClk (sd_clk_i),
