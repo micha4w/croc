ff5531de04fe3bbf1145d0dd07cdde3bf58ce718
micha4w <micha4w@gmail.com>
2025-06-04 17:30:49 +0200

    refactor: clean up sd_clk_en code for dat_wrap, merge both shiftregs into one, fix reset

--- rtl/user_domain/sdhci/dat_write/crc16_write_copy.sv
+++ rtl/user_domain/sdhci/dat_write/crc16_write_copy.sv
@@ -16,14 +16,10 @@ module crc16_write (
   logic dat_i_xor_out;
 
   always_comb begin : crc16_comb
-    lower_5_d   = lower_5_q;
-    middle_7_d  = middle_7_q;
-    upper_4_d   = upper_4_q;
-
-    crc_ser_o     = upper_4_q[3];
-    dat_i_xor_out = upper_4_q[3] ^ dat_ser_i;
+    dat_i_xor_out = 1'b0;
 
     if (shift_out_crc16_i) begin : shift_out_result
+      crc_ser_o       =  upper_4_q[3];
        upper_4_d[3:1] =  upper_4_q[2:0];
        upper_4_d[0]   = middle_7_q[6];
       middle_7_d[6:1] = middle_7_q[5:0];
@@ -31,6 +27,7 @@ module crc16_write (
        lower_5_d[4:1] =  lower_5_q[3:0];
        lower_5_d[0]   = 1'b0; //shift in zeros while shifting out crc, no reset needed.
     end else begin : calc_crc16
+      dat_i_xor_out   =  upper_4_q[3] ^ dat_ser_i;
        upper_4_d[3:1] =  upper_4_q[2:0];
        upper_4_d[0]   = middle_7_q[6] ^ dat_i_xor_out;
       middle_7_d[6:1] = middle_7_q[5:0];
